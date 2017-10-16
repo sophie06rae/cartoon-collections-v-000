b2d8497335ce492cbd7d2ae14b6baefc6030bb3e
Sophie Rae <sophie.rae.06@gmail.com>
2017-10-16 12:02:25 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array.detect {|i| i == "cheddar" }
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  if array.include? { |i| i == "cheddar" }
+    puts i 
+  end 
 end
