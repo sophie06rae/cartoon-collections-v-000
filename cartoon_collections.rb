d519aa0e93ac83485b0aa7ec44bd4c9d4b3feed1
Sophie Rae <sophie.rae.06@gmail.com>
2017-10-16 10:47:05 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,12 +7,12 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.each do |i| 
+  array.each do |i|
     if array.any? i.to_i < 4
       return true
     else array.all? i.to_i < 4
       return false
-  end 
+  end
 end
 
 def find_the_cheese# code an argument here
