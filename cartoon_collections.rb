c31ab887acc09d7ac0b2ccf91526725cb608e297
Sophie Rae <sophie.rae.06@gmail.com>
2017-10-16 10:22:56 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.map {|i| i[0].upcase i[1..-1] << "!"}
+  array.map {|i| i[0].upcase << "!"}
 end
 
 def long_planeteer_calls# code an argument here
